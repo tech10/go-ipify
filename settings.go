e9d4c00380c3be8137671da4f71d7f0ae15c5e7a
Blake G <blakeblind@gmail.com>
2021-03-12 08:25:42 -0700

    Make every variable that will never change into a constant.

--- settings.go
+++ settings.go
@@ -13,15 +13,10 @@ const VERSION = "1.0.1"
 const MAX_TRIES = 3
 
 // This is the ipify service base URI.  This is where all API requests go.
-var API_URI = "https://api.ipify.org"
+const API_URI = "https://api.ipify.org"
 
 // The user-agent string is provided so that I can (eventually) keep track of
 // what libraries to support over time.  EG: Maybe the service is used
 // primarily by Windows developers, and I should invest more time in improving
 // those integrations.
-var USER_AGENT = fmt.Sprintf(
-	"go-ipify/%s go/%s %s",
-	VERSION,
-	runtime.Version()[2:],
-	strings.Title(runtime.GOOS),
-)
+var USER_AGENT = fmt.Sprintf("go-ipify/%s go/%s %s", VERSION, runtime.Version()[2:], strings.Title(runtime.GOOS))
