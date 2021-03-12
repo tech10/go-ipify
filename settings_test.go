ffad4443011255002c22026981abecb91a79ff45
Blake G <blakeblind@gmail.com>
2021-03-12 07:26:52 -0700

    Modify settings test to print the user agent string on pass.

--- settings_test.go
+++ settings_test.go
@@ -1,17 +1,18 @@
 package ipify
 
 import (
+	"fmt"
 	"runtime"
 	"strings"
 	"testing"
 )
 
-func TestUserAgent(t *testing.T) {
+func Test_UserAgent(t *testing.T) {
 	if !strings.Contains(strings.ToLower(USER_AGENT), runtime.GOOS) {
 		t.Error("USER_AGENT doesn't contain the OS type:", USER_AGENT)
 	}
-
 	if !strings.Contains(USER_AGENT, VERSION) {
 		t.Error("USER_AGENT doesn't contain the library version:", USER_AGENT)
 	}
+	fmt.Println("User agent: " + USER_AGENT)
 }
